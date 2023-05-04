3f7f85282da33f0ab3d20f365f9765a14f59dce7
nick.liao <altoliaw@hotmail.com>
2023-05-04 15:54:22 +0800

    Modifying the export syntax

--- Headers/Entry.h
+++ Headers/Entry.h
@@ -13,8 +13,8 @@
 #include "Header.h"
 
 #if defined(_WIN32) || defined(_WIN64)
-  __attribute__((visibility("default"))) int ___encryption(const unsigned char*, const int, unsigned char*, int*, unsigned char*, unsigned char*);
-  __attribute__((visibility("default"))) int ___decryption(unsigned char*, int, unsigned char*, int*, unsigned char*, unsigned char*);
+  __declspec(dllexport) int ___encryption(const unsigned char*, const int, unsigned char*, int*, unsigned char*, unsigned char*);
+  __declspec(dllexport) int ___decryption(unsigned char*, int, unsigned char*, int*, unsigned char*, unsigned char*);
 #else
   __attribute__((visibility("default"))) int ___encryption(const unsigned char*, const int, unsigned char*, int*, unsigned char*, unsigned char*);
   __attribute__((visibility("default"))) int ___decryption(unsigned char*, int, unsigned char*, int*, unsigned char*, unsigned char*);
